78af7ec1873eab8960142bc99816536e58523fc7
Dilshan Oshada <86113915+SLDrago@users.noreply.github.com>
2023-12-03 00:18:59 +0530

    init commit

---
+++ src/app/pages/item/item.component.ts
@@ -0,0 +1,23 @@
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+  constructor(private param:ActivatedRoute,private service:OrderDetailsService) {}
+  getMenuId:any;
+  menuData:any;
+  ngOnInit(): void {
+    this.getMenuId = this.param.snapshot.paramMap.get('id');
+    console.log(this.getMenuId,'getmenu');
+    if(this.getMenuId){
+      this.menuData = this.service.foodDetails.filter((value)=>{
+        return value.id==this.getMenuId
+      });
+    }
+  }
+}
